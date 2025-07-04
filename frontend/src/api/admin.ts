9ed86595f5907f38c6b8f7499a94fbdb555ec38f
ICE-awa <HaydenIce@outlook.com>
2025-07-04 13:35:03 +0800

    tmp: 暂时提交代码

--- frontend/src/api/admin.ts
+++ frontend/src/api/admin.ts
@@ -6,10 +6,28 @@ interface GetUsersResponse {
     content: User[];
 }
 
+interface BulkAddUserPayload {
+    username: string;
+    password: string;
+    role?: 'USER' | 'ADMIN';
+}
+
+interface BulkDeleteUserPayload {
+    usernames: string[];
+}
+
 export const getUsers = () => {
     return axiosInstance.get<GetUsersResponse>('/admin/users');
 }
 
 export const deleteUser = (userId: number) => {
     return axiosInstance.delete(`/admin/users/${userId}`);
+}
+
+export const bulkAddUsers = (users: BulkAddUserPayload[]) => {
+    return axiosInstance.post('/admin/users/batch-add', users);
+}
+
+export const bulkDeleteUsers = (data: BulkDeleteUserPayload) => {
+    return axiosInstance.post('/admin/users/batch-delete', data);
 }
