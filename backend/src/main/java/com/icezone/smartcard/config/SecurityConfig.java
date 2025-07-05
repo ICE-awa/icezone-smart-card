ec9f1dbb61fe2fa7a715319fa3b0f5f74af8b81e
ICE-awa <HaydenICE@outlook.com>
2025-07-05 16:58:05 +0800

    bugfix: 修复了 JWT tokens 过期后没有重定向的 bug

--- backend/src/main/java/com/icezone/smartcard/config/SecurityConfig.java
+++ backend/src/main/java/com/icezone/smartcard/config/SecurityConfig.java
@@ -1,10 +1,6 @@
 package com.icezone.smartcard.config;
 
-
-import com.icezone.smartcard.security.JwtAuthenticationFilter;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,7 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.Customizer;
+
+import com.icezone.smartcard.security.JwtAuthenticationEntryPoint;
+import com.icezone.smartcard.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +23,9 @@ public class SecurityConfig {
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -41,6 +42,9 @@ public class SecurityConfig {
         http
             .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
+            .exceptionHandling(exception -> exception
+                .authenticationEntryPoint(unauthorizedHandler)
+            )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
